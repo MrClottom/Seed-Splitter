90a6ac468330fd4b65f056c7bcca7ae57a7302ea
MrClottom <philippe-dumonet@protonmail.com>
2020-07-31 16:43:24 +0200

    Remove unnecessary line from main.js

--- main.js
+++ main.js
@@ -2,7 +2,3 @@
   loadWordList('bip39-en', '#recombination .wordlist > textarea')
   loadWordList('bip39-en', '#secretSplit .wordlist > textarea ')
 })()
-
-const register = (selector, as, func, sendTo) => {
-  const el = document.querySelector(selector)
-}
