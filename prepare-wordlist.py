5f004208960ccc2333cdfb4e73b418f6f0c359fb
MrClottom <philippe-dumonet@protonmail.com>
2020-07-31 16:10:13 +0200

    Remove unnecessary python file

--- prepare-wordlist.py
+++
@@ -1,9 +0,0 @@
-with open('wordlist.txt', 'r') as f:
-	words = f.read()
-
-prepared = words.replace('\n', "',\n  '", 2047)
-
-with open('wordlist.js', 'w') as f:
-	f.write(f"const wordList = [\n  '{prepared[:-1]}'\n]")
-
-print('done please check result before using')
